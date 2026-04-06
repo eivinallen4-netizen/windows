1aaab40a8305985fc1e92f34fdd23b1a06b572d5
theodore-12 <theodore.414476@nv.ccsd.net>
2026-04-06 15:31:46 -0700

    vercel anylitics bot

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next"
 import "./globals.css";
 
 function getMetadataBase() {
@@ -51,6 +52,7 @@ export default function RootLayout({
       <body className="antialiased" suppressHydrationWarning>
         {children}
         <SpeedInsights />
+        <Analytics/>
       </body>
     </html>
   );
