23981b7b87288ab5a3ebf1ac6f57c7a8f73f9877
theodore-12 <theodore.414476@nv.ccsd.net>
2026-04-16 11:09:11 -0700

    sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,6 +2,18 @@
 const config = {
   siteUrl: process.env.SITE_URL || 'https://purebinlv.com',
   generateRobotsTxt: true,
+  exclude: [
+    '/admin/',
+    '/rep/',
+    '/tech/',
+    '/api/',
+    '/setup/',
+    '/schedule',
+    '/save-quote',
+    '/quote',
+    '/script',
+    
+  ],
 }
 
 export default config
